bdd2c32c4fa6a072feabe22d4abec74eeafb6dc0
bl2i4n <bl2i4n@yahoo.com>
2016-12-22 15:04:11 -0800

    accidently did checkpoint19 on master

--- scripts/album.js
+++ scripts/album.js
@@ -1,49 +1,3 @@
-// Example Album
- var albumPicasso = {
-     title: 'The Colors',
-     artist: 'Pablo Picasso',
-     label: 'Cubism',
-     year: '1881',
-     albumArtUrl: 'assets/images/album_covers/01.png',
-     songs: [
-         { title: 'Blue', duration: '4:26' },
-         { title: 'Green', duration: '3:14' },
-         { title: 'Red', duration: '5:01' },
-         { title: 'Pink', duration: '3:21'},
-         { title: 'Magenta', duration: '2:15'}
-     ]
- };
- 
- // Another Example Album
- var albumMarconi = {
-     title: 'The Telephone',
-     artist: 'Guglielmo Marconi',
-     label: 'EM',
-     year: '1909',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Hello, Operator?', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Fits in your pocket', duration: '3:21'},
-         { title: 'Can you hear me now?', duration: '3:14' },
-         { title: 'Wrong phone number', duration: '2:15'}
-     ]
- };
-
-var albumJT = {
-     title: '20/20',
-     artist: 'Justin Timberlake',
-     label: 'EM',
-     year: '2015',
-     albumArtUrl: 'assets/images/album_covers/20.png',
-     songs: [
-         { title: 'Suit & Tie', duration: '1:01' },
-         { title: 'Ring, ring, ring', duration: '5:01' },
-         { title: 'Im bringing sexy back', duration: '3:21'},
-         { title: 'Senorita', duration: '3:14' },
-         { title: 'Cabaret', duration: '2:15'}
-     ]
- };
 
  // allows access to the data held in the attribute using DOM methods
 var createSongRow = function(songNumber, songName, songLength){
@@ -60,19 +14,21 @@ var createSongRow = function(songNumber, songName, songLength){
     var clickHandler = function() {
         var songNumber = $(this).attr('data-song-number');
         
-        if(currentlyPlayingSong !== null) {
+        if(currentlyPlayingSongNumber !== null) {
             //Revert to song number for currently playing song because user started playing new song.
-            var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-            currentlyPlayingCell.html(currentlyPlayingSong);
+            var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+            currentlyPlayingCell.html(currentlyPlayingSongNumber);
         }
-        if(currentlyPlayingSong !== songNumber){
+        if(currentlyPlayingSongNumber !== songNumber){
             // Switch from Play -> Pause button to indicate new song playing.
             $(this).html(pauseButtonTemplate);
-            currentlyPlayingSong = songNumber;
-        } else if (currentlyPlayingSong === songNumber) {
+            currentlyPlayingSongNumber = songNumber;
+            currentSongFromAlbum = currentAlbum.songs[songNumber -1];
+        } else if (currentlyPlayingSongNumber === songNumber) {
             // Switch from Pause -> Play button to pause currently playing song.
             $(this).html(playButtonTemplate);
-            currentlyPlayingSong = null;
+            currentlyPlayingSongNumber = null;
+            currentSongFromAlbum = null;
         }
     };
     
@@ -80,7 +36,7 @@ var createSongRow = function(songNumber, songName, songLength){
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');
         
-        if(songNumber !== currentlyPlayingSong){
+        if(songNumber !== currentlyPlayingSongNumber){
             songNumberCell.html(playButtonTemplate);
         }
     };
@@ -88,7 +44,7 @@ var createSongRow = function(songNumber, songName, songLength){
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');
         
-        if(songNumber !== currentlyPlayingSong){
+        if(songNumber !== currentlyPlayingSongNumber){
             songNumberCell.html(songNumber);
         }
     };
@@ -111,7 +67,8 @@ var $albumSongList = $('.album-view-song-list');
     
 
 var setCurrentAlbum = function(album){
-
+    currentAlbum = album;
+    
     //  Assign values to each part of the album (text, images)
     $albumTitle.text(album.title);
     $albumArtist.text(album.artist);
@@ -126,6 +83,12 @@ var setCurrentAlbum = function(album){
     }
 };
 
+var updatePlayerBarSong = function(songName){
+    $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+    $('.currently-playing .artist-name').text(currentAlbum.artist);
+    $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);   
+}
+
 //need if statement here for checkpoint 13!
 //review what this does
 //mathematical expression
@@ -134,8 +97,11 @@ var setCurrentAlbum = function(album){
 // Album button templates
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+var currentAlbum = null;
 //Store state of playing songs
-var currentlyPlayingSong = null;
+var currentPlayingSongNumber = null;
+var currentSongFromAlbum = null;
 
 $(document).ready(function() {
     setCurrentAlbum(albumPicasso);   
